4347ebf62e6c3c40014aef8251d5562dc18e3365
FTPeters <37051132+FTPeters@users.noreply.github.com>
2018-03-07 14:45:11 +0100

    Update ViewLocal.java

--- ProgramProj/src/ForUni/ViewLocal.java
+++ ProgramProj/src/ForUni/ViewLocal.java
@@ -3,9 +3,14 @@ package ForUni;
 import java.awt.Color;
 import java.awt.Graphics;
 
+/**
+ * The View that implements the Local view (on the Screen).
+ */
 public class ViewLocal extends View {
 	
-	@Override
+	/**
+	 * When called this updates the Local view.
+	 */
 	public void draw(Graphics g, Color[][] picture, int pixelSizeX, int pixelSizeY) {
 		for (int x = 0; x < picture.length; x++)
 			for (int y = 0; y < picture[0].length; y++)
