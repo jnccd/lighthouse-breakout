7a97e7c72f39cc62aec0a1d24ef0d5f96f420c46
FTPeters <37051132+FTPeters@users.noreply.github.com>
2018-03-07 14:46:19 +0100

    Updated Comments

--- ProgramProj/src/ForUni/ViewLocal.java
+++ ProgramProj/src/ForUni/ViewLocal.java
@@ -7,14 +7,13 @@ import java.awt.Graphics;
  * The View that implements the Local view (on the Screen).
  */
 public class ViewLocal extends View {
-	
+
 	/**
 	 * When called this updates the Local view.
 	 */
 	public void draw(Graphics g, Color[][] picture, int pixelSizeX, int pixelSizeY) {
 		for (int x = 0; x < picture.length; x++)
-			for (int y = 0; y < picture[0].length; y++)
-			{
+			for (int y = 0; y < picture[0].length; y++) {
 				g.setColor(picture[x][y]);
 				g.fillRect(x * pixelSizeX, y * pixelSizeY, pixelSizeX, pixelSizeY);
 			}
