160c87475b9e504e485d4164b9f69ca7e23888a9
FTPeters <37051132+FTPeters@users.noreply.github.com>
2018-03-07 14:47:02 +0100

    Updated Comments

--- ProgramProj/src/ForUni/ViewLightHouse.java
+++ ProgramProj/src/ForUni/ViewLightHouse.java
@@ -4,13 +4,24 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.io.IOException;
 
+/**
+ * The View-class that implements the Lighthouse.
+ */
 public class ViewLightHouse extends View {
 
 	private LighthouseDisplay display;
 
+	/**
+	 * Constructs a new LighthouseView with given Username and API-Token.
+	 * 
+	 * @param username
+	 *            the Username of the Account
+	 * @param token
+	 *            the API-Token (has to be the latest version)
+	 */
 	public ViewLightHouse(String username, String token) {
 		display = new LighthouseDisplay(username, token);
-		
+
 		try {
 			display.connect();
 		} catch (Exception e) {
@@ -19,23 +30,32 @@ public class ViewLightHouse extends View {
 		}
 	}
 
-	
-	
+	/**
+	 * Encodes a 2-dimensional Color-array to a byte-array
+	 * 
+	 * @param colors
+	 *            the 2-dim. Color-array
+	 */
 	private void updateColors(Color[][] colors) {
-		byte[] data = new byte[14*28*3];
+		byte[] data = new byte[14 * 28 * 3];
 		int counter = 0;
-		
-		for(int y = 0; y < 14; y++) {
-			for(int x = 0; x < 28; x++) {
-				data[counter] = (byte) colors[x][y].getRed();
-				data[counter + 1] = (byte) colors[x][y].getGreen();
-				data[counter + 2] = (byte) colors[x][y].getBlue();
-				counter += 3;
+
+		for (int y = 0; y < 14; y++) {
+			for (int x = 0; x < 28; x++) {
+				data[counter++] = (byte) colors[x][y].getRed();
+				data[counter++] = (byte) colors[x][y].getGreen();
+				data[counter++] = (byte) colors[x][y].getBlue();
 			}
 		}
 		sendData(data);
 	}
-	
+
+	/**
+	 * Sends the Data (Colors) to the lighthouse.
+	 * 
+	 * @param data
+	 *            Colors encoded in a byte-array
+	 */
 	private void sendData(byte[] data) {
 		try {
 			display.send(data);
@@ -45,9 +65,9 @@ public class ViewLightHouse extends View {
 		}
 	}
 
-
-
-	@Override
+	/**
+	 * Draws the given Color-array to the lighthouse.
+	 */
 	public void draw(Graphics g, Color[][] picture, int pixelSizeX, int pixelSizeY) {
 		updateColors(picture);
 	}
