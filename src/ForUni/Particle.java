617ea35f4873255edb3b03f298372cdac9b5130f
stu211717 <stu211717@boromir>
2018-03-09 13:29:03 +0100

    Finished comments (finally)

--- src/ForUni/Particle.java
+++ src/ForUni/Particle.java
@@ -2,12 +2,27 @@ package ForUni;
 
 import java.awt.Color;
 
+/**
+ * Implements particles
+ */
 public class Particle {
 	ParticleManager parent;
 	Vector2 pos, vel, origin;
 	Color col;
 	int maxX, maxY, lifeTimer = 0, lifeTime;
 	
+	/**
+	 * Builds new particle object
+	 * 
+	 * @param pos start position
+	 * @param vel start velocity
+	 * @param col color
+	 * @param parent manager
+	 * @param maxX X-Border
+	 * @param maxY Y-Border
+	 * @param lifeTime The time until the particle is deleted in frames
+	 * @param origin coordinates of the object this originates from
+	 */
 	public Particle(Vector2 pos, Vector2 vel, Color col, ParticleManager parent, int maxX, int maxY, int lifeTime, Vector2 origin)
 	{
 		this.pos = pos;
@@ -20,10 +35,14 @@ public class Particle {
 		this.origin = origin;
 	}
 	
+	/**
+	 * computes particle logic
+	 */
 	public void update()
 	{
 		lifeTimer++;
 		
+		// border collision
 		if (pos.X < 0 && vel.X < 0)
 			vel.X *= -1;
 		if (pos.X > maxX && vel.X > 0)
@@ -35,16 +54,24 @@ public class Particle {
 		
 		//Vector2 toOrigin = origin.Subtract(pos);
 		//vel = vel.Add(new Vector2(toOrigin.Y, -toOrigin.X / 2).Divide(50 * toOrigin.LengthSquared()));
+		
+		// Gravity
 		vel.Y += 0.01f;
 		
+		// Screen particle friction
 		vel = vel.Multiply(0.993f);
 		pos = pos.Add(vel);
 		
+		// death
 		if (lifeTimer > lifeTime)
 			parent.removeList.add(this);
 	}
 	
-	void draw(Color[][] Picture)
+	/**
+	 * draws particle to the specific canvas
+	 * @param Picture the canvas object
+	 */
+	public void draw(Color[][] Picture)
 	{
 		if ((int)pos.X > 0 && (int)pos.X < Picture.length && (int)pos.Y > 0 && (int)pos.Y < Picture[0].length)
 			Picture[(int)pos.X][(int)pos.Y] = Extensions.lerp(col, Color.BLACK, lifeTimer / (float)lifeTime);
--- src/ForUni/ParticleManager.java
+++ src/ForUni/ParticleManager.java
@@ -3,12 +3,20 @@ package ForUni;
 import java.awt.Color;
 import java.util.ArrayList;
 
+/**
+ * Manages a bunch of particles
+ */
 public class ParticleManager {
 	Color[][] superPicture;
 	ArrayList<Particle> particles;
 	int superSamplingMult;
 	public ArrayList<Particle> removeList;
 	
+	/**
+	 * Creates new particle manager object
+	 * @param originalPicture original canvas
+	 * @param superSamplingMult super sampling multiplier
+	 */
 	public ParticleManager(Color[][] originalPicture, int superSamplingMult)
 	{
 		this.superSamplingMult = superSamplingMult;
@@ -21,36 +29,65 @@ public class ParticleManager {
 		particles = new ArrayList<Particle>();
 		removeList = new ArrayList<Particle>();
 	}
-	
+	/**
+	 * Turns a brick into a bunch of particles
+	 * @param b the original brick object
+	 * @param ba the evil ball who destroyed the innocent brick
+	 */
 	public void turnToParticles(Brick b, Ball ba)
 	{
+		// Clone the innocent brick and draw it to a fake screen so we can use those 
+		// fake screen pixels later to color our particles
 		Brick clone = new Brick(0, 0, b.getHP() + 1, null, b.spwansBall);
 		Color[][] fakePicture = new Color[Brick.width][Brick.height];
 		clone.draw(fakePicture);
 		
+		// particle rectangle width and height
 		int w = Brick.width * superSamplingMult;
 		int h = Brick.height * superSamplingMult;
 		
+		// Add the particles 
 		for (int x = 0; x < w; x++)
 			for (int y = 0; y < h; y++)
-				particles.add((new Particle(new Vector2(b.x * superSamplingMult + x, b.y * superSamplingMult + y),  
-						new Vector2(x - w / 2f, (y - h / 4f) * 1f).Divide(Extensions.rdm.nextFloat() * 10 + 3f), 
+	
+				particles.add((new Particle(
+						// starting position
+						new Vector2(b.x * superSamplingMult + x, b.y * superSamplingMult + y),
+						// starting velocity (away from the middle of the brick)
+						new Vector2(x - w / 2f, (y - h / 4f)).Divide(Extensions.rdm.nextFloat() * 10 + 3f), 
 						//new Vector2(0, 0), 
-						//new Vector2((x - w / 2f) * (x - w / 2f), (y - h / 2f) * (y - h / 2f)).Divide(15), 
-						fakePicture[x / superSamplingMult][y / superSamplingMult], this, 
-						superPicture.length, superPicture[0].length, 80, 
+						//new Vector2((x - w / 2f) * (x - w / 2f), (y - h / 2f) * (y - h / 2f)).Divide(15),
+						
+						// color of the part of the brick the particle starts in
+						fakePicture[x / superSamplingMult][y / superSamplingMult],
+						// manager for this particle
+						this, 
+						// super sampled canvas width and height
+						superPicture.length, superPicture[0].length,
+						// particle lifetime in frames
+						80, 
+						// center of the spawning brick
 						new Vector2(b.x * superSamplingMult + w / 2f, b.y * superSamplingMult + h / 2f))));
 	}
+	/**
+	 * clear the super sampled canvas
+	 */
 	private void clearSuper()
 	{
 		for (int x = 0; x < superPicture.length; x++)
 			for (int y = 0; y < superPicture[0].length; y++)
 				superPicture[x][y] = Color.BLACK;
 	}
+	/**
+	 * clear the particle list
+	 */
 	public void clear() {
 		particles.clear();
 	}
 	
+	/**
+	 * update all the particles
+	 */
 	public void update()
 	{
 		for (int i = 0; i < particles.size(); i++)
@@ -59,13 +96,18 @@ public class ParticleManager {
 		particles.removeAll(removeList);
 	}
 	
+	/**
+	 * draw all the particles
+	 * @param c canvas
+	 */
 	public void draw(Color[][] c)
 	{
+		// draw to the super sampled canvas
 		clearSuper();
-		
 		for (int i = 0; i < particles.size(); i++)
 			particles.get(i).draw(superPicture);
 		
+		// draw the super sampled canvas to the original canvas
 		for (int i = 0; i < c.length; i++)
 			for (int j = 0; j < c[0].length; j++)
 			{
